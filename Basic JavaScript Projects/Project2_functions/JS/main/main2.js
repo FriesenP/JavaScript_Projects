78c189e959bde6df7eb3da3937e6365ab68d17b9
FriesenP <127798370+FriesenP@users.noreply.github.com>
2023-05-15 22:05:56 -0700

    Basic JavaScript project assignment, Project2_functions assignment

---
+++ Basic JavaScript Projects/Project2_functions/JS/main/main2.js
@@ -0,0 +1,14 @@
+//window.alert method
+window.alert("hello, world!");
+
+//documnet.write method
+document.write("For the last time, hello, world!");
+
+//creating two variables
+var Sent1 = "This is the beginnning of the string";
+var Sent2 = " and this is the end of the string";
+
+//displaying the variables using the document.write(Sent1 + Sent2);
+
+//an expression
+document.write(33*84);
